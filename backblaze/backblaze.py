13e7f7c55068a378ea44eb26c4dd31e2e9738724
Mrinal Sinha <themrinalsinha@gmail.com>
2018-10-15 01:50:14 +0530

    .

--- backblaze/backblaze.py
+++ backblaze/backblaze.py
@@ -52,7 +52,7 @@ class Backblaze(object):
         self.upload_url        = request.json()['uploadUrl']
         self.upload_auth_token = request.json()['authorizationToken']
 
-    def upload(self, bucket_name, upload_file, author_name = 'Unknown'):
+    def upload(self, bucket_name, upload_file, author_name='Unknown'):
         file_data = open(upload_file, 'rb').read()
         sha1_file = sha1(file_data).hexdigest()
         file_name = path.basename(upload_file)
